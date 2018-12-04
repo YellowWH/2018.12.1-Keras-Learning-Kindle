6e670038f08b43ead969a2ab8293b78c7f95ca1a
YellowWH <41254936+YellowWH@users.noreply.github.com>
2018-12-04 17:25:41 +0900

    Add files via upload

---
+++ 7.1/Inception V3.py
@@ -0,0 +1,20 @@
+import keras
+from keras import layers
+
+#假设我们有一个4维的输出张量
+branch_a = layers.Conv2D(128, 1, activation='relu', strides=2)(x)
+
+branch_b = layers.Conv2D(128, 1, activation='relu')(x)
+branch_b = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_b)
+
+branch_c = layers.AveragePooling2D(3, strides=2)(x)
+branch_c = layers.Conv2D(128, 3, activation='relu')(branch_c)
+
+branch_d = layers.Conv2D(128, 1, activation='relu')(x)
+branch_d = layers.Conv2D(128, 3, activation='relu')(branch_d)
+branch_d = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_d)
+
+output = layers.concatenate([branch_a, branch_b, branch_c, branch_d], axis=-1)
+
+# 也可以直接在keras里面引用,有在ImageNet上训练的到的权重
+# keras.applications.inception_v3.InceptionV3
